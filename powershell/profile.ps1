793a476c4ea47ab2c02284b45a37da5df22802aa
Bernardo Falé <mbfale@ua.pt>
2026-07-10 11:46:52 +0100

    CHanging neovim config

--- powershell/profile.ps1
+++
@@ -1,8 +0,0 @@
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "/Users/bernardo/miniconda/bin/conda") {
-    (& "/Users/bernardo/miniconda/bin/conda" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-#endregion
-
